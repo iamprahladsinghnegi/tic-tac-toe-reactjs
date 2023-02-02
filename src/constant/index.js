b782b9a9860e065b34221972d3435085b340e368
Prahlad Singh Negi <mecoolpankajnegi@gmail.com>
2023-02-02 10:41:56 +0530

    fix: updated backend server url

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,1 +1,1 @@
-export const SOCKET_ENDPOINT = "https://tictactoenodejs.herokuapp.com" // 'http://localhost:3003';
+export const SOCKET_ENDPOINT = "https://tic-tac-toe-67a4.onrender.com" // 'http://localhost:3003';
